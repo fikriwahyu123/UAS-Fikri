d02e6545f8921d5b8201f522e3f6628f0b3cf92a
fikriwahyu123 <113502848+fikriwahyu123@users.noreply.github.com>
2023-01-27 22:55:58 +0700

    Add files via upload

---
+++ pcs_2101082036_B_service/src/main/java/com/fikri/pcs_2101082036_B_service/Pcs2101082036BServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fikri.pcs_2101082036_B_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pcs2101082036BServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pcs2101082036BServiceApplication.class, args);
+	}
+
+}
---
+++ pcs_2101082036_B_service/src/test/java/com/fikri/pcs_2101082036_B_service/Pcs2101082036BServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fikri.pcs_2101082036_B_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pcs2101082036BServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
